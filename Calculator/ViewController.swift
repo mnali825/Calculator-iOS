8401dcf1bab3b268568e126f39740c78872bdfad
HerbaN <mna271@nyu.edu>
2016-04-04 12:50:46 -0400

    complete 4 function calc

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,19 +7,99 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Addition = "+"
+        case Empty = "Empty"
+        case Equal = "="
+    }
+    
+    @IBOutlet weak var outputLbl: UILabel!
+    
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    var result = ""
+    var currentOperation: Operation = Operation.Empty
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func numberPressed(btn: UIButton!) {
+        runningNumber += "\(btn.tag)"
+        outputLbl.text = runningNumber
+        
     }
 
 
+    @IBAction func onDividePressed(sender: AnyObject) {
+        processOperation(Operation.Divide)
+    }
+    
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+        processOperation(Operation.Multiply)
+    }
+    
+    @IBAction func onSubtractPressed(sender: AnyObject) {
+        processOperation(Operation.Subtract)
+    }
+    
+    @IBAction func onAdditionPressed(sender: AnyObject) {
+        processOperation(Operation.Addition)
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject) {
+        processOperation(currentOperation)
+    }
+    
+    @IBAction func onClearPressed(sender: AnyObject) {
+        leftValStr = ""
+        rightValStr = ""
+        result = ""
+        currentOperation = Operation.Empty
+        outputLbl.text = "0"
+    }
+    func processOperation(op: Operation) {
+        if currentOperation != Operation.Empty {
+            
+            //if leftValStr == 123456789 && currentOperation == Operation.Equal
+            if runningNumber != "" {
+                rightValStr = runningNumber
+                runningNumber = ""
+                
+                if currentOperation == Operation.Multiply {
+                    result = "\(Double(leftValStr)! * Double(rightValStr)!)"
+                }
+                else if currentOperation == Operation.Divide {
+                    result = "\(Double(leftValStr)! / Double(rightValStr)!)"
+                }
+                else if currentOperation == Operation.Subtract {
+                    result = "\(Double(leftValStr)! - Double(rightValStr)!)"
+                }
+                else if currentOperation == Operation.Addition {
+                    result = "\(Double(leftValStr)! + Double(rightValStr)!)"
+                }
+                
+                leftValStr = result
+                outputLbl.text = result
+            }
+            currentOperation = op
+        }
+        else {
+            leftValStr = runningNumber
+            runningNumber = ""
+            currentOperation = op
+            
+        }
+        
+    }
 }
 
